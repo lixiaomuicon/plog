89097f6008c2e12c6f2f5115723509a83322c2c9
李小沐 <122504161+lixiaomuicon@users.noreply.github.com>
2023-08-17 01:24:40 +0800

    Add files via upload

--- utlit/bootstrap3.3.5/js/password.js
+++ utlit/bootstrap3.3.5/js/password.js
@@ -1,12 +1,10 @@
-function check() {
-    var pass = document.getElementById("pass").value;
-    if (pass == "xiaomu") {
-        alert("欢迎来到时光相册");
-        // window.document.f.action = "html/album.html";
-        // window.document.f.submit();
+function myFunction() {
+    var i, j;
+    i = "lixiaomu";
+    j = document.getElementById("password").value;
+    if (j == i) {
+        location.href = "xiaomu.html";
     } else {
-        alert("密码错误");
-
+        alert("密码错误！请重新输入！");
     }
-
 }
